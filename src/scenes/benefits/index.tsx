aa728c43eba3b7404bd729364331e815ff7b9b31
YeffSWD <yeframos@gmail.com>
2023-11-03 00:17:15 -0600

    first commit

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,145 @@
+import HText from "@/shared/HText";
+import { BenefitType, SelectedPage } from "@/shared/types";
+import {
+    HomeModernIcon,
+    UserGroupIcon,
+    AcademicCapIcon
+} from "@heroicons/react/24/solid";
+import { motion } from 'framer-motion';
+import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from '@/assets/BenefitsPageGraphic.png'
+
+const benefits: Array<BenefitType> = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6"/>,
+        title: "State of the Art Facilities",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,"
+    },
+    {
+        icon: <UserGroupIcon className="h-6 w-6"/>,
+        title: "100's of Diverse Classes",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,"
+    },
+    {
+        icon: <AcademicCapIcon className="h-6 w-6"/>,
+        title: "Expert and Pro Trainers",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,"
+    },
+];
+
+const container = {
+    hidden: {},
+    visible: {
+        transition: { staggerChildren: 0.2}
+    }
+}
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Benefits = ({setSelectedPage}: Props) => {
+  return (
+    <section
+    id="benefits"
+    className="mx-auto min-h-full w-5/6 py-20"
+    >
+        <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+        >
+            {/** Header */}
+            <motion.div className="md:my-5 md:w-3/5"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true, amount: 0.5}}
+            transition={{ duration: 0.5 }}
+            variants={{
+                hidden: {opacity: 0, x:-50},
+                visible: {opacity:1, x:0}
+            }}
+            >
+                <HText>MORE THAN JUST A GYM</HText>
+                <p className="my-5 text-sm">
+                    We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true training care into each and every member.
+                </p>
+            </motion.div>
+            {/** Benefits */}
+            <motion.div className="md:flex items-center justify-between gap-8 mt-5"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true, amount: 0.5}}
+            variants={container}
+            >
+                {benefits.map((benefit: BenefitType) => (
+                    <Benefit 
+                        key={benefit.title}
+                        icon={benefit.icon}
+                        title={benefit.title}
+                        description={benefit.description}
+                        setSelectedPage={setSelectedPage}
+                    />
+                ))}
+            </motion.div>
+            {/** Graphics and Description */}
+                <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+                    {/** Graphic */}
+                    <img 
+                    className="mx-auto"
+                    src={BenefitsPageGraphic}
+                    alt="benefits-page-graphic" />
+
+                    {/** Description */}
+                    <div>
+                        {/** Title */}
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{once: true, amount: 0.5}}
+                            transition={{ duration: 0.5 }}
+                            variants={{
+                                hidden: {opacity: 0, x:-50},
+                                visible: {opacity:1, x:0}
+                            }}
+                        >
+                            <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+                                <div>
+                                    <HText>
+                                        MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                                        <span className="text-primary-500">Fit.</span>
+                                    </HText>
+                                </div>
+                            </div>
+                        </motion.div>
+
+                        {/** Description */}
+                        <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{once: true, amount: 0.5}}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: {opacity: 0, x:-50},
+                            visible: {opacity:1, x:0}
+                        }}
+                        >
+                            <p className="my-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <p className="mb-5">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </motion.div>
+
+                        {/** Button */}
+                        <div className="relative mt-16">
+                            <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                                <ActionButton setSelectedPage={setSelectedPage}>
+                                    Join Now
+                                </ActionButton>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </motion.div>
+    </section>
+  )
+}
+
+export default Benefits
---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,34 @@
+import Logo from '@/assets/Logo.png'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer className='bg-primary-100 py-16'>
+        <div className='justify-content mx-auto w-5/6 gap-16 md:flex'>
+            <div className='mt-16 basis-1/2 md:mt-0'>
+                <img src={Logo} alt="logo" />
+                <p className='my-5'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+                <p>
+                    © EvoGym All Rights Reserved.
+                </p>
+            </div>
+            <div className='mt-16 basis-1/4 md:mt-0'>
+                <h4 className='font-bold'>Links</h4>
+                <p className='my-5'>Ipsum is simply dummy text of the printing and type</p>
+                <p className='my-5'>Lorem Ipsum has been the industry's standard dummy</p>
+                <p>text ever since the 1500s, when an unknown printer took</p>
+            </div>
+            <div className='mt-16 basis-1/4 md:mt-0'>
+                <h4 className='font-bold'>Contact Us</h4>
+                <p className='my-5'>Ipsum is simply dummy text of the printing and type</p>
+                <p>32689-45878</p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
